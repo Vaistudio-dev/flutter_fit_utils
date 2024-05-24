007db3fb4f27b182fdbd23f3b424428c5a8537b7
s0punk <simonoliviervaill@gmail.com>
2024-05-24 08:17:25 -0400

    Removed out of context extensions.

--- lib/extensions/double_extension.dart
+++ lib/extensions/double_extension.dart
@@ -2,16 +2,6 @@ import 'package:flutter_fit_utils/flutter_fit_utils.dart';
 
 /// Extension of the [double] type.
 extension FitDoubleExtension on double {
-  /// Converts a [double] to a [String]. Only prints the decimals if there is any.
-  String toBetterString({int decimals = 1}) {
-    final String decimalPart = toString().split(".").last;
-    if (decimalPart.split("").any((element) => element != "0")) {
-      return toStringAsFixed(decimals);
-    }
-
-    return toStringAsFixed(0);
-  }
-
   /// Creates a [Model] from a [double].
   /// Encapsulates the data inside model->data->"data".
   Model toModel({String id = "", String userId = ""}) => Model(
--- lib/extensions/index.dart
+++ lib/extensions/index.dart
@@ -1,7 +1,4 @@
-export 'color_extension.dart';
-export 'datetime_extension.dart';
 export 'double_extension.dart';
-export 'duration_extension.dart';
 export 'modelable_extension.dart';
 export 'string_extension.dart';
 export 'int_extension.dart';
